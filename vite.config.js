9c6935f60aa279bd212d06202521f06e1a67303b
MaxenceChan <123729633+MaxenceChan@users.noreply.github.com>
2025-09-29 19:10:11 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// If you deploy on GitHub Pages, set base to '/<repo-name>/'.
-// Example: base: '/workout-tracker/'
-export default defineConfig({
-  plugins: [react()],
-  // base: '/workout-tracker/',
-})
