875db7c3470faeab2a3631e1ad45123514e8e162
MaxenceChan <123729633+MaxenceChan@users.noreply.github.com>
2025-12-16 00:55:52 +0100

    Update steps.js
    Signed-off-by: MaxenceChan <123729633+MaxenceChan@users.noreply.github.com>

--- api/steps.js
+++ api/steps.js
@@ -32,16 +32,8 @@ export default async function handler(req, res) {
     const fitness = google.fitness({ version: "v1", auth: oauth2Client });
 
     const end = Date.now();
-    const oldestSnap = await userRef
-      .collection("steps")
-      .orderBy("date")
-      .limit(1)
-      .get();
+    const start = new Date("2025-01-01").getTime();
     
-    const start = oldestSnap.empty
-      ? new Date("2015-01-01").getTime()
-      : new Date(oldestSnap.docs[0].id).getTime();
-
     const response = await fitness.users.dataset.aggregate({
       userId: "me",
       requestBody: {
