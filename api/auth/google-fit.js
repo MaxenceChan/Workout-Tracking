ebf4558b31024736af1e28a6315d1eaea74dd7fc
MaxenceChan <123729633+MaxenceChan@users.noreply.github.com>
2025-12-15 11:42:29 +0100

    Update google-fit.js
    Signed-off-by: MaxenceChan <123729633+MaxenceChan@users.noreply.github.com>

--- api/auth/google-fit.js
+++ api/auth/google-fit.js
@@ -1,3 +1,28 @@
+import { google } from "googleapis";
+
 export default function handler(req, res) {
-  res.status(200).send("API AUTH OK");
+  const { uid } = req.query;
+
+  if (!uid) {
+    return res.status(400).send("Missing uid");
+  }
+
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GOOGLE_CLIENT_ID,
+    process.env.GOOGLE_CLIENT_SECRET,
+    process.env.GOOGLE_REDIRECT_URI
+  );
+
+  const scopes = [
+    "https://www.googleapis.com/auth/fitness.activity.read",
+  ];
+
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    prompt: "consent",
+    scope: scopes,
+    state: uid, // UID Firebase plus tard
+  });
+
+  res.redirect(url);
 }
